5827d4742a557b0505d9481a4737bfe956e3f1ab
tetradog <tetradog@gmail.com>
2018-03-17 05:27:48 +0100

    to 2.0

---
+++ TiposComplejos/ByteArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gabriel.Cat.S.Binaris
+{
+    public class ByteArrayBinario : ElementoIListBinario<byte>
+    {
+        public ByteArrayBinario(LongitudBinaria unidadCantidadElementos = LongitudBinaria.UInt) : base(new ElementoBinarioTamañoFijo(Utilitats.Serializar.TiposAceptados.Byte), unidadCantidadElementos)
+        {
+        }
+
+        public ByteArrayBinario( byte[] marcaFin) : base(new ElementoBinarioTamañoFijo(Utilitats.Serializar.TiposAceptados.Byte), marcaFin)
+        {
+        }
+    }
+}
